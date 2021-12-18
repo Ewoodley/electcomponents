67edbd2325a73f18e580d92b41d63ded1f7292ab
Eric Woodley <noctisumbra@gmail.com>
2021-12-18 02:17:07 -0600

    Add new component to manage dropdowns

--- src/Components/resistors.js
+++ src/Components/resistors.js
@@ -1,28 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Dropdown, Option } from "./dropdown";
 /*import styles from './resistors.modules.css'
  */
-export default class App extends Component {
-  render() {
-    return (
-      <div style={{ backgroundColor: "	#F5F5DC" }}>
-        <h1>This page is about Resistors</h1>
-        <p>
-          Resistors, at the most basic, limit the amount of current flowing
-          through a circuit
-        </p>
-        <p>
-          To determine which end to read from, the end with the largest gap
-          between bands is the bottom of the resistor
-        </p>
-        <p>Use the following chart to determine the value of the resistor:</p>
-        <br></br>
-        <select name="selectList" id="selectList">
-          <option value="band 1">Band 1</option>
-          <option value="band 2">Band 2</option>
-          <option value="band 3">Band 3</option>
-          <option value="band 4">Band 4</option>
-        </select>
-      </div>
-    );
+
+export default function App() {
+  const [state, setState] = useState({
+    band1: "",
+    band2: "",
+    band3: "",
+    band4: "",
+  });
+
+  function handleSelect(event) {
+    const value = event.target.value;
+    setState({
+      ...state,
+      [event.target.name]: value,
+    });
+    console.log(value);
   }
+  return (
+    <div style={{ backgroundColor: "	#F5F5DC" }}>
+      <h1>This page is about Resistors</h1>
+      <p>
+        Resistors, at the most basic, limit the amount of current flowing
+        through a circuit
+      </p>
+      <p>
+        To determine which end to read from, the end with the largest gap
+        between bands is the bottom of the resistor
+      </p>
+      <p>Use the following chart to determine the value of the resistor:</p>
+
+      <Dropdown
+        formLabel="Band 1 color"
+        onChange={handleSelect}
+        value={state.band1}
+      >
+        <Option value="Red" />
+        <Option value="Black" />
+        <Option value="Blue" />
+        <Option value="Green" />
+      </Dropdown>
+      <Dropdown
+        formLabel="Band 2 color"
+        onChange={handleSelect}
+        value={state.band2}
+      >
+        <Option value="Red" />
+        <Option value="Black" />
+        <Option value="Blue" />
+        <Option value="Green" />
+      </Dropdown>
+      <Dropdown
+        formLabel="Band 3 color"
+        onChange={handleSelect}
+        value={state.band3}
+      >
+        <Option value="Red" />
+        <Option value="Black" />
+        <Option value="Blue" />
+        <Option value="Green" />
+      </Dropdown>
+      <Dropdown
+        formLabel="Band 4 color"
+        onChange={handleSelect}
+        value={state.band4}
+      >
+        <Option value="Red" />
+        <Option value="Black" />
+        <Option value="Blue" />
+        <Option value="Green" />
+      </Dropdown>
+      <p>Resistor value is: </p>
+    </div>
+  );
 }
