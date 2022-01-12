f226831d22b72e7b672ae764a017e7e384eecea9
Eric Woodley <noctisumbra@gmail.com>
2022-01-12 16:26:23 -0600

    Removing unused files, updates to Readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 First, Semantic UI and Styled Components will need to be installed.
 
-To install these items, run npm install semantic-ui-react semantic-ui-css and npm install styled-components.
+To install these items, run npm install semantic-ui-react semantic-ui-css
 
 After installing the requisite components, then run npm start to run a dev build of the SPA in your selected web browser
--- src/Components/resistors.js
+++ src/Components/resistors.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Dropdown } from "semantic-ui-react";
-import {ohms} from './ohms'
+import { ohms } from "./ohms";
 import { multiplier } from "./multiplier";
 import { tolerance } from "./tolerance";
 
 export default function Resistors() {
-  
-
-  
   return (
     <div>
       <h1>This page is about Resistors</h1>
@@ -21,34 +18,22 @@ export default function Resistors() {
       </p>
       <p>Use the following chart to determine the value of the resistor:</p>
 
-      <Dropdown
-        placeholder="Band 1 color"
-       fluid
-       selection
-       options={ohms}
-      />
-      
-       <Dropdown
-         placeholder="Band 2 color"
-         fluid
-         selection
-         options={ohms}
-       />
-        
+      <Dropdown placeholder="Band 1 color" fluid selection options={ohms} />
+
+      <Dropdown placeholder="Band 2 color" fluid selection options={ohms} />
+
       <Dropdown
         placeholder="Band 3 color - Multiplier"
         fluid
         selection
         options={multiplier}
       />
-          <Dropdown
+      <Dropdown
         placeholder="Band 4 color - Tolerance"
         fluid
         selection
         options={tolerance}
       />
-           
     </div>
   );
-
 }
