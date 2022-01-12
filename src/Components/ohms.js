7f034e88b68b8e8c6b464759bed0ce244c07582e
Ewoodley82 <noctisumbra@gmail.com>
2022-01-12 06:34:13 -0600

    updates to Restistor styling

---
+++ src/Components/ohms.js
@@ -0,0 +1,51 @@
+export const ohms = [{
+    key: "Black - 0Ω",
+    text: "Black - 0Ω",
+    value: "Black - 0Ω"
+  },
+  {
+    key: "Brown - 1Ω",
+    text: "Brown - 1Ω",
+    value: "Brown - 1Ω"
+  },
+  {
+    key: "Red - 2Ω",
+    text: "Red - 2Ω",
+    value: "Red - 2Ω"
+  },
+  {
+    key: "Orange - 3Ω",
+    text: "Orange - 3Ω",
+    value: "Orange - 3Ω"
+  },
+  {
+    key: "Yellow - 4Ω",
+    text: "Yellow - 4Ω",
+    value: "Yellow - 4Ω"
+  },
+  {
+    key: "Green - 5Ω",
+    text: "Green - 5Ω",
+    value: "Green - 5Ω"
+  },
+  {
+    key: "Blue - 6Ω",
+    text: "Blue - 6Ω",
+    value: "Blue - 6Ω"
+  },
+  {
+    key: "Violet - 7Ω",
+    text: "Violet - 7Ω",
+    value: "Violet - 7Ω"
+  },
+  {
+    key: "Grey - 8Ω",
+    text: "Grey - 8Ω",
+    value: "Grey - 8Ω"
+  },
+  {
+    key: "White - 9Ω",
+    text: "White - 9Ω",
+    value: "White - 9Ω"
+  }
+];
--- src/Components/resistors.js
+++ src/Components/resistors.js
@@ -1,26 +1,15 @@
-import React, { useState } from "react";
-import { Dropdown, Option } from "./dropdown";
-/*import styles from './resistors.modules.css'
- */
+import React from "react";
+import { Dropdown } from "semantic-ui-react";
+import {ohms} from './ohms'
+import { multiplier } from "./multiplier";
+import { tolerance } from "./tolerance";
 
 export default function Resistors() {
-  const [state, setState] = useState({
-    band1: "",
-    band2: "",
-    band3: "",
-    band4: "",
-  });
+  
 
-  function handleSelect(event) {
-    const value = event.target.value;
-    setState({
-      ...state,
-      [event.target.name]: value,
-    });
-    console.log(value);
-  }
+  
   return (
-    <div style={{ backgroundColor: "	#F5F5DC" }}>
+    <div>
       <h1>This page is about Resistors</h1>
       <p>
         Resistors, at the most basic, limit the amount of current flowing
@@ -33,72 +22,32 @@ export default function Resistors() {
       <p>Use the following chart to determine the value of the resistor:</p>
 
       <Dropdown
-        formLabel="Band 1 color"
-        onChange={handleSelect}
-        value={state.band1}
-      >
-        <Option value="" />
-        <Option value="Black - 0Ω " />
-        <Option value="Brown - 1Ω" />
-        <Option value="Red - 2Ω" />
-        <Option value="Orange - 3Ω" />
-        <Option value="Yellow - 4Ω" />
-        <Option value="Green - 5Ω" />
-        <Option value="Blue - 6Ω" />
-        <Option value="Violet - 7Ω" />
-        <Option value="Grey - 8Ω" />
-        <Option value="White - 9Ω" />
-      </Dropdown>
+        placeholder="Band 1 color"
+       fluid
+       selection
+       options={ohms}
+      />
+      
+       <Dropdown
+         placeholder="Band 2 color"
+         fluid
+         selection
+         options={ohms}
+       />
+        
       <Dropdown
-        formLabel="Band 2 color"
-        onChange={handleSelect}
-        value={state.band2}
-      >
-        <Option value="" />
-        <Option value="Black - 0Ω " />
-        <Option value="Brown - 1Ω" />
-        <Option value="Red - 2Ω" />
-        <Option value="Orange - 3Ω" />
-        <Option value="Yellow - 4Ω" />
-        <Option value="Green - 5Ω" />
-        <Option value="Blue - 6Ω" />
-        <Option value="Violet - 7Ω" />
-        <Option value="Grey - 8Ω" />
-        <Option value="White - 9Ω" />
-      </Dropdown>
-      <Dropdown
-        formLabel="Band 3 color - Multiplier"
-        onChange={handleSelect}
-        value={state.band3}
-      >
-        <Option value="" />
-        <Option value="Black - *1" />
-        <Option value="Brown - *10" />
-        <Option value="Red - *100" />
-        <Option value="Orange - *1,000" />
-        <Option value="Yellow - *10,000" />
-        <Option value="Green - *100,000" />
-        <Option value="Blue - *1,000,000" />
-        <Option value="Violet - *10,000,000" />
-        <Option value="Gold - *.1" />
-        <Option value="Silver - *.01" />
-      </Dropdown>
-      <Dropdown
-        formLabel="Band 4 color - Tolerance"
-        onChange={handleSelect}
-        value={state.band4}
-      >
-        <Option value="" />
-        <Option value="none - 20%" />
-        <Option value="Brown - 1%" />
-        <Option value="Green - .5%" />
-        <Option value="Blue - .25%" />
-        <Option value="Violet - .1%" />
-        <Option value="Grey - .05%" />
-        <Option value="Gold - 5%" />
-        <Option value="Silver -  10%" />
-      </Dropdown>
-     
+        placeholder="Band 3 color - Multiplier"
+        fluid
+        selection
+        options={multiplier}
+      />
+          <Dropdown
+        placeholder="Band 4 color - Tolerance"
+        fluid
+        selection
+        options={tolerance}
+      />
+           
     </div>
   );
 
